0784e6d8d56bb5c04edbd2ebf6822dbadaff81c8
Ella <ellathitotran@gmail.com>
2023-09-20 20:25:05 -0500

    put together userroutes, schema, userjs, index,js, js public folder, utils folder, and seeds js.

---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connect.js');
+
+
+// Create the Handlebars engine
+const hbs = exphbs.create({
+    // Specify your configuration options here
+    defaultLayout: 'main', // This sets the default layout to 'main.handlebars'
+    });
+    
+    app.engine('handlebars', hbs.engine);
+    app.set('view engine', 'handlebars');
+   
+   // Define your routes and middleware here
+   
+   app.get('/', (req, res) => {
+    res.render('index'); // Renders the 'index.handlebars' template
+   });
+   
+   app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+   });
+   
