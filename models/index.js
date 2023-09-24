63dd57e259606bd72147a688080d0bb0140cdec2
k8 <katesterchi@gmail.com>
2023-09-25 01:03:13 +0200

    got the login page to load

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-const User = require ('./user');
-const Plant = require('./plant');
+const User = require ('./User');
+// const Plant = require('./plant');
 
-User.hasMany(Plant, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// User.hasMany(Plant, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-Plant.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Plant.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-module.exports = { User, Plants };
+module.exports = { User };
